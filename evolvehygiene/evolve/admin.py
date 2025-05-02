2da297e2d25e73dea57a74f644d1c8f31c900b7f
JiyaGilbert <jiyajiya8812@gmail.com>
2025-05-02 16:15:07 +0530

    models added

--- evolvehygiene/evolve/admin.py
+++ evolvehygiene/evolve/admin.py
@@ -1,4 +1,5 @@
 from .models import ContactMessage, Gallery
+from .models import Category, SubCategory, Product
 from django.contrib import admin
 # from .models import Gallery
 
@@ -8,3 +9,40 @@ admin.site.register(Gallery)
 @admin.register(ContactMessage)
 class ContactMessageAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'subject', 'submitted_at')
+
+
+
+
+class SubCategoryInline(admin.TabularInline):  # or admin.StackedInline
+    model = SubCategory
+    extra = 1  # Number of empty forms to display
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    inlines = [SubCategoryInline]  # Show subcategories when editing category
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'category__name')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'offer_price', 'subcategory', 'rating')
+    list_filter = ('subcategory__category', 'subcategory')
+    search_fields = ('name', 'details')
+    readonly_fields = ('rating',)  # If rating is calculated automatically
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('name', 'details', 'subcategory')
+        }),
+        ('Pricing', {
+            'fields': ('price', 'offer_price')
+        }),
+        ('Other Details', {
+            'fields': ('weight', 'rating', 'vector')
+        }),
+    )
--- evolvehygiene/evolve/models.py
+++ evolvehygiene/evolve/models.py
@@ -6,18 +6,52 @@ from django import forms
 
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='categories/', blank=True, null=True)
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    
     def __str__(self):
         return self.name
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategories')
+    name = models.CharField(max_length=100)
+    
+    class Meta:
+        unique_together = ('category', 'name')  # Ensures subcategory names are unique within a category
+    
+    def __str__(self):
+        return f"{self.category.name} - {self.name}"
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    details = models.TextField()
+    weight = models.DecimalField(max_digits=10, decimal_places=2, help_text="Weight in kilograms")
+    offer_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.SET_NULL, null=True, blank=True, related_name='products')
+    rating = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+    vector = models.JSONField(blank=True, null=True)  # For storing vector embeddings
+    
+    def __str__(self):
+        return self.name
+
+
+
+
+# class Category(models.Model):
+#     name = models.CharField(max_length=255)
+#     image = models.ImageField(upload_to='categories/', blank=True, null=True)
+
+#     def __str__(self):
+#         return self.name
 
 class Gallery(models.Model):
     feedimage = models.FileField()
     name = models.CharField(max_length=100)
     model=models.CharField(max_length=400)
-
+    category = models.CharField(max_length=100, default='uncategorized')                                                                                                                                                                
     price = models.DecimalField(max_digits=10, decimal_places=2)  
     # delivary = models.CharField(max_length=100)
     
